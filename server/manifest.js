5f09d4958d44d84940ac989162b0ae176cb9d036
teddmason <teddmason@gmail.com>
2016-08-19 13:07:15 +0100

    Errbit integration

--- server/manifest.js
+++ server/manifest.js
@@ -23,6 +23,16 @@ const manifest = {
         register: 'good',
         options: config.logging
       }
+    },
+    {
+      plugin: {
+        register: 'node-hapi-airbrake',
+        options: {
+          key: config.envVars.fmp_service_errbit_key,
+          env: 'production',
+          host: config.envVars.fmp_service_errbit_host
+        }
+      }
     }
   ]
 }
