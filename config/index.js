515937ba8551b971d38b5f011d2a7328103b574d
EADEV <davidjamesstone@gmail.com>
2018-01-25 17:13:21 +0000

    Update config schema validator

--- config/index.js
+++ config/index.js
@@ -2,10 +2,13 @@ const Joi = require('joi')
 const schema = require('./schema')
 const config = require('./server.json')
 
-const result = Joi.validate(config, schema)
+// Validate config
+const result = Joi.validate(config, schema, { abortEarly: false })
 
+// Throw if config is invalid
 if (result.error) {
   throw new Error('The server config is invalid. ' + result.error.message)
 }
 
+// Return the config
 module.exports = result.value
