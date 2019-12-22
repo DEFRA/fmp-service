c2f3a5fc2bef696e53a8152297210c1dde982a1a
Harsh Vasudev <hvasudev@gmail.com>
2019-12-22 01:14:06 +0000

    Fixed indentation problem to run tests

--- server/routes/zones-data.js
+++ server/routes/zones-data.js
@@ -1,23 +1,22 @@
-var fs = require('fs');
+var fs = require('fs')
 const Boom = require('boom')
 module.exports = {
-    method: 'GET',
-    path: '/zones/{zoneNumber}',
-    options: {
-        description: 'Based on Zone Number passed, it will send static data as HTML',
-        handler: async (request, h) => {
-            try {
-                var zoneNumber = request.params.zoneNumber;
-                var filePath = process.env.PWD + `/data/flood-zones/${zoneNumber}.html`;
-                if (fs.existsSync(filePath)) {
-                    return fs.readFileSync(filePath, { encoding: 'utf-8' });
-                } else {
-                    return Boom.badImplementation('Error occured in getting the file location');
-                }
-            }
-            catch (err) {
-                return Boom.badImplementation('Error occured in getting the Zones data')
-            }
+  method: 'GET',
+  path: '/zones/{zoneNumber}',
+  options: {
+    description: 'Based on Zone Number passed, it will send static data as HTML',
+    handler: async (request, h) => {
+      try {
+        var zoneNumber = request.params.zoneNumber
+        var filePath = process.env.PWD + `/data/flood-zones/${zoneNumber}.html`
+        if (fs.existsSync(filePath)) {
+          return fs.readFileSync(filePath, { encoding: 'utf-8' })
+        } else {
+          return Boom.badImplementation('Error occured in getting the file location')
         }
+      } catch (err) {
+        return Boom.badImplementation('Error occured in getting the Zones data')
+      }
     }
+  }
 }
