e9ad069655e3ef3623912b04ba2e595bc2921f93
Hari Gillala <defra.test.warrington@gmail.com>
2019-12-23 20:15:48 +0000

    route file added for zones-data

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ module.exports = [
   require('./is-england'),
   require('./test-db'),
   require('./zones'),
+  require('./zones-data'),
   require('./zones-by-polygon'),
   require('./standard-text'),
   require('./standard-text-by-section')]
