ab0153b8cff0364d9addd17a5bc045d8f6d5cab9
Hari Gillala <defra.test.warrington@gmail.com>
2020-02-06 20:15:57 +0000

    added the request body to the trigger

--- HttpSendTrigger/index.js
+++ HttpSendTrigger/index.js
@@ -1,30 +1,6 @@
-// module.exports = async function (context, req) {
-//   context.log('JavaScript HTTP trigger function processed a request.')
-
-//   if (req.query.name || (req.body && req.body.name)) {
-//     context.res = {
-//       // status: 200, /* Defaults to 200 */
-//       body: 'Hello ' + (req.query.name || req.body.name)
-//     }
-//   } else {
-//     context.res = {
-//       status: 400,
-//       body: 'Please pass a name on the query string or in the request body'
-//     }
-//   }
-// }
-
 module.exports = async function (context, req) {
-  var message
-  if (req.query.name || (req.body && req.body.name)) {
-    message = 'Hello ' + (req.query.name || req.body.name)
-  } else {
-    message = 'Service Bus queue message created at ' + Date.now()
-  }
-
-  context.log(message)
+  context.log(req.body)
   context.bindings.outputSbQueue = []
-  context.bindings.outputSbQueue.push('1 ' + message)
-  context.bindings.outputSbQueue.push('2 ' + message)
+  context.bindings.outputSbQueue.push('1' + req.body)
   context.done()
 }
