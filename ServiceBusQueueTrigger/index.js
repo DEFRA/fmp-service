4743667a27a521914cc4672918df6e8b1c76fc09
Harsh Vasudev <hvasudev@gmail.com>
2020-02-19 19:14:13 +0000

    FCRM-1244 map service bus queue message to readable parameters

--- ServiceBusQueueTrigger/index.js
+++ ServiceBusQueueTrigger/index.js
@@ -1,3 +1,5 @@
 module.exports = async function (context, mySbMsg) {
   context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg)
+  // const receivedPayload = JSON.parse(JSON.stringify(mySbMsg))
+  // const {x, y, customerName, customerEmailAddress, applicationReferenceNumber} = receivedPayload
 }
