b7053f9f8a34a43c8fb84c9bfe0563f27232610f
Hari Gillala <defra.test.warrington@gmail.com>
2020-02-20 13:27:39 +0000

    updated the body to capture the images with no data

--- server/services/pdfmake/body.js
+++ server/services/pdfmake/body.js
@@ -3,9 +3,15 @@ const fs = require('fs');
 async function body(rawdataASArray) {
     try {
         var data = rawdataASArray.map((item) => {
-            if (!item.error)
-                return [`Map Type: ${item.title},  Data Produced On: ${item.modellingDate} \n\n`, { image: item.imageUrl, height: 500, width: 500 }]
-            return 'no data'
+            if (!item.error) {
+                if (item.imageUrl !== '') {
+                    return [`Map Type: ${item.title},  Data Produced On: ${item.modellingDate} \n\n`, { image: item.imageUrl, height: 500, width: 500}]
+                }
+                else {
+                    return [`Map Type: ${item.title}`, 'No data available \n\n']
+                }
+            }
+            return ''
         })
 
         return data;
