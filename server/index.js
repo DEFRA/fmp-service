d1fc9f6fbdc93ef9f77eb54e31da0c70366d44f8
Mark Fee <Mark.Fee@defra.gov.uk>
2022-04-07 12:35:33 +0100

    FCRM-3636 removed the glupe dependency

--- server/index.js
+++ server/index.js
@@ -1,8 +1,32 @@
-const manifest = require('./manifest')
+const hapi = require('@hapi/hapi')
+const config = require('../config')
 
-module.exports = {
-  options: {
-    relativeTo: __dirname
-  },
-  manifest: manifest
+async function createServer () {
+  const server = hapi.server({
+    port: config.server.port,
+    host: config.server.host,
+    routes: {
+      validate: {
+        options: {
+          abortEarly: false
+        }
+      }
+    }
+  })
+
+  await server.register(require('./plugins/log-errors'))
+  await server.register(require('./plugins/router'))
+  await server.register(require('./plugins/logging'))
+
+  if (config.errbit.postErrors) {
+    delete config.errbit.postErrors
+    await server.register({
+      plugin: require('node-hapi-airbrake'),
+      options: config.errbit
+    })
+  }
+
+  return server
 }
+
+module.exports = createServer
