514bc0b129143454b6e2b74a25fa64b5a5df7999
Mark Fee <mark.fee@defra.gov.uk>
2024-10-09 16:06:20 +0100

    FCRM-5208 Created health-check and version.js, parameterised Dockerfile version (#74)
    * FCRM-5208 Created health-check and version.js
    
    * FCRM-5208 parameterised dockerfile version.js
    
    * FCRM-5208 removed unused parameters to satisfy sonarcloud code checks
    
    * FCRM-5208 added test/config.js to cover exception code
    
    * FCRM-5208 suppressed caught error log in is-england test
    
    * FCRM-5208 added a unit test to cover health-check
    
    * FCRM-5208 ensure unit test for health-check will pass inside a docker container

---
+++ test/routes/health-check.js
@@ -0,0 +1,32 @@
+const Lab = require('@hapi/lab')
+const lab = exports.lab = Lab.script()
+const Code = require('@hapi/code')
+const Hapi = require('@hapi/hapi')
+const { version } = require('../../version')
+
+const server = Hapi.Server({
+  host: 'localhost',
+  port: 8050
+})
+
+server.route(require('../../server/routes/health-check'))
+
+lab.experiment('health-check', () => {
+  lab.before(async () => {
+    await server.start()
+  })
+
+  lab.after(async () => {
+    await server.stop()
+  })
+
+  lab.test('health-check', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/health-check'
+    })
+    Code.expect(response.statusCode).to.equal(200)
+    const { payload } = response
+    Code.expect(payload).to.equal(`{"name":"fmp-service","version":"${version}"}`)
+  })
+})
